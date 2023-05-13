1b3b8bcf3ae04faa5934e03ea19f4c4fdfa91154
Shubham Lal <lal.shubham027@gmail.com>
2023-05-14 00:55:17 +0530

    Added CORS

--- api/server.js
+++ api/server.js
@@ -10,6 +10,12 @@ app.use(express.json());
 app.use("/", express.static("uploads"));
 
 // CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Set-Cookie: cross-site-cookie=whatever; SameSite=None; Secure");
+  next();
+});
 const corsOptions = {
     origin: '*',
     credentials: true,
